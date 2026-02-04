b67bc3a085523ea2d736751fc0bd27897cd031e2
Lo <lovisaornestal@gmail.com>
2026-02-04 14:25:03 +0100

    changed about me text

--- src/main.ts
+++ src/main.ts
@@ -109,7 +109,7 @@ function finalSlide() {
 
     const aboutMeText = document.createElement("p");
     aboutMeText.textContent =
-      "I'm currently studying frontend development, learning to translate visual concepts into working code. Background in art direction for fashion and tech. Now I want to focus on building things people can actually use.";
+      "Former art director now studying frontend development. Background in fashion and tech. I get a kick out of seeing code turn into real things online. Now I want to build stuff people actually use, while continuing to grow technically";
 
     aboutMeText.className =
       "mt-6 text-white font-medium tracking-tight leading-relaxed text-sm sm:text-base w-full sm:min-w-[280px] md:min-w-[320px] max-w-prose text-left break-words hyphens-auto";
